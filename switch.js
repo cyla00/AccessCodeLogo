73d9656fa12e3afd51ec9968482c0e80b77385c3
cyla00 <cyla00@protonmail.com>
2020-05-12 15:28:50 +0200

    darkmode

--- switch.js
+++ switch.js
@@ -1,12 +1,27 @@
+var white = 'white';
+var black = 'black';
 
-var change = document.getElementById('button').addEventListener('click', () => {
-  const white = 'white'
-  const black = 'black'
+var change = document.getElementById('btn1').addEventListener('click', (toggle) => {
   document.body.style.backgroundColor = white;
   document.body.style.color = black;
   document.getElementById("inline").style.borderBottomColor = black;
-  document.getElementById('button').style.backgroundColor = white;
-  document.getElementById('button').style.color = black;
-  document.getElementById('bar1').style.color = "#000000";
-  document.getElementById('bar2').style.color = "#000000";
+  document.getElementById('btn1').style.backgroundColor = white;
+  document.getElementById('btn1').style.color = black;
+  document.getElementById('btn2').style.backgroundColor = white;
+  document.getElementById('btn2').style.color = black;
+  document.getElementById('bar1').style.color = black;
+  document.getElementById('bar2').style.color = black;
+  document.getElementById('code').style.color = white;
+})
+var change = document.getElementById('btn2').addEventListener('click', (toggle) => {
+  document.body.style.backgroundColor = black;
+  document.body.style.color = white;
+  document.getElementById("inline").style.borderBottomColor = white;
+  document.getElementById('btn1').style.backgroundColor = black;
+  document.getElementById('btn1').style.color = white;
+  document.getElementById('btn2').style.backgroundColor = black;
+  document.getElementById('btn2').style.color = white;
+  document.getElementById('bar1').style.color = white;
+  document.getElementById('bar2').style.color = white;
+  document.getElementById('code').style.color = white;
 })
