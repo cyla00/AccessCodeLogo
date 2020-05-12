8cbcc447e00245ac4cc90e816b87561f00246a79
cyla00 <cyla00@protonmail.com>
2020-05-12 14:16:55 +0200

    javascript singleColor switch

---
+++ switch.js
@@ -0,0 +1,33 @@
+
+
+var change = document.getElementById('button').addEventListener('click', (back, bocolor, bordercolor, stycolor, butback) => {
+  const white = 'white'
+  const black = 'black'
+  this.back = document.body.style.backgroundColor = white;
+  this.bocolor = document.body.style.color = black;
+  this.bordercol = document.getElementById("inline").style.borderBottomColor = black;
+  this.butback = document.getElementById('button').style.backgroundColor = white;
+  this.stycolor = document.getElementById('button').style.color = black;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.body.style.backgroundColor = white;
+// document.body.style.color = black;
+// document.getElementById("inline").style.borderBottomColor = black;
+// document.getElementById('button').style.backgroundColor = white;
+// document.getElementById('button').style.color = black;
