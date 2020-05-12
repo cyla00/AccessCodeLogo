68946ee1a434a6de353c91b38a064a67b04b75b5
cyla00 <cyla00@protonmail.com>
2020-05-12 17:30:27 +0200

    minor

--- switch.js
+++ switch.js
@@ -1,27 +1,34 @@
-var white = 'white';
-var black = 'black';
+let white = 'white';
+let black = 'black';
+let count = 0;
 
-var change = document.getElementById('btn1').addEventListener('click', (toggle) => {
-  document.body.style.backgroundColor = white;
-  document.body.style.color = black;
-  document.getElementById("inline").style.borderBottomColor = black;
-  document.getElementById('btn1').style.backgroundColor = white;
-  document.getElementById('btn1').style.color = black;
-  document.getElementById('btn2').style.backgroundColor = white;
-  document.getElementById('btn2').style.color = black;
-  document.getElementById('bar1').style.color = black;
-  document.getElementById('bar2').style.color = black;
-  document.getElementById('code').style.color = white;
-})
-var change = document.getElementById('btn2').addEventListener('click', (toggle) => {
-  document.body.style.backgroundColor = black;
-  document.body.style.color = white;
-  document.getElementById("inline").style.borderBottomColor = white;
-  document.getElementById('btn1').style.backgroundColor = black;
-  document.getElementById('btn1').style.color = white;
-  document.getElementById('btn2').style.backgroundColor = black;
-  document.getElementById('btn2').style.color = white;
-  document.getElementById('bar1').style.color = white;
-  document.getElementById('bar2').style.color = white;
-  document.getElementById('code').style.color = white;
-})
+
+
+function turnON(){
+  if(count === 0){
+    count = 1;
+    document.body.style.backgroundColor = white;
+    document.body.style.color = black;
+    document.getElementById("inline").style.borderBottomColor = black;
+    document.getElementById('btn1').style.backgroundColor = white;
+    document.getElementById('btn1').style.color = black;
+    document.getElementById('btn2').style.backgroundColor = white;
+    document.getElementById('btn2').style.color = black;
+    document.getElementById('bar1').style.color = black;
+    document.getElementById('bar2').style.color = black;
+    document.getElementById('code').style.color = white;
+  }
+  else{
+    count = 0;
+    document.body.style.backgroundColor = black;
+    document.body.style.color = white;
+    document.getElementById("inline").style.borderBottomColor = white;
+    document.getElementById('btn1').style.backgroundColor = black;
+    document.getElementById('btn1').style.color = white;
+    document.getElementById('btn2').style.backgroundColor = black;
+    document.getElementById('btn2').style.color = white;
+    document.getElementById('bar1').style.color = white;
+    document.getElementById('bar2').style.color = white;
+    document.getElementById('code').style.color = white;
+  }
+}
