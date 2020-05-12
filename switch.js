72154098411f05bf756d2e0227e01b7bdb70147c
cyla00 <cyla00@protonmail.com>
2020-05-12 14:37:23 +0200

    minor

--- switch.js
+++ switch.js
@@ -1,33 +1,12 @@
 
-
-var change = document.getElementById('button').addEventListener('click', (back, bocolor, bordercolor, stycolor, butback) => {
+var change = document.getElementById('button').addEventListener('click', () => {
   const white = 'white'
   const black = 'black'
-  this.back = document.body.style.backgroundColor = white;
-  this.bocolor = document.body.style.color = black;
-  this.bordercol = document.getElementById("inline").style.borderBottomColor = black;
-  this.butback = document.getElementById('button').style.backgroundColor = white;
-  this.stycolor = document.getElementById('button').style.color = black;
+  document.body.style.backgroundColor = white;
+  document.body.style.color = black;
+  document.getElementById("inline").style.borderBottomColor = black;
+  document.getElementById('button').style.backgroundColor = white;
+  document.getElementById('button').style.color = black;
+  document.getElementById('bar1').style.color = "#000000";
+  document.getElementById('bar2').style.color = "#000000";
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.body.style.backgroundColor = white;
-// document.body.style.color = black;
-// document.getElementById("inline").style.borderBottomColor = black;
-// document.getElementById('button').style.backgroundColor = white;
-// document.getElementById('button').style.color = black;
